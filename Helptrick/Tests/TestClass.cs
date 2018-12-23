07e0b693124a761430cfa04ddc598797c0b423da
Ricardo Fonseca <ricardo.jfonseca@outlook.com>
2018-12-23 23:16:07 +0000

    Playing around with UT

---
+++ Helptrick/Tests/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Helptrick.Tests
+{
+    class TestClass
+    {
+        [TestFixture, Category("LongRunning")]
+        public class Test
+        {
+            [Test, Category("Long")]
+            public void Test1()
+            {
+                Assert.AreEqual("teste2", ViewModel.TestClass.GetString());
+
+            }
+        }
+    }
+}
