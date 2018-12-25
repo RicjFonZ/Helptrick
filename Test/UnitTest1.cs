31e3949850bc74051ba5acba0ecd27a038f56f21
Ricardo Fonseca <ricardo.jfonseca@outlook.com>
2018-12-25 19:17:07 +0000

    Playing with UT

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+            Assert.AreEqual(1, 1);
+
+        }
+    }
+}
