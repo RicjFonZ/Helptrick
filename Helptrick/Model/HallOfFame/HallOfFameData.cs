f58e164f100309538c88c74400ca597d8bc7d62f
Ricardo Fonseca <ricardo.jfonseca@outlook.com>
2018-12-27 19:58:25 +0000

    Working on data model

---
+++ Helptrick/Model/HallOfFame/HallOfFameData.cs
@@ -0,0 +1,16 @@
+﻿using Helptrick.Model.CommonData;
+using System.Collections.Generic;
+
+namespace Helptrick.Model.HallOfFame
+{
+    /// <summary>
+    ///     Hall of Fame Players
+    /// </summary>
+    internal class HallOfFameData : BaseHattrickData
+    {
+        /// <summary>
+        ///     Container for the data about the player.
+        /// </summary>
+        public List<Player> Players { get; set; } = new List<Player>();
+    }
+}
